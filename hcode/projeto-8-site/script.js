9fdc35e06bc9f535516f93d706d0f043d5c0b942
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-15 23:48:46 -0300

    projeto 8 site finalizado

--- hcode/projeto-8-site/script.js
+++ hcode/projeto-8-site/script.js
@@ -1,1 +1,11 @@
-export default[]
+document.querySelector('.open-menu').addEventListener('click', e => {
+    document.querySelector('header .menu').classList.add('open')
+})
+
+document.querySelector('.close-menu').addEventListener('click', e => {
+    document.querySelector('header .menu').classList.remove('open')
+})
+
+document.querySelector('.menu .backdrop').addEventListener('click', e => {
+    document.querySelector('header .menu').classList.remove('open')
+})
